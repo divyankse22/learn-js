1761cbf702ee73a2d0237a10f5dae61affd44bc7
Divyank Srivastava <121336426+divyankse22@users.noreply.github.com>
2024-11-04 10:56:01 +0000

    basics and datatypes

---
+++ 01_basics/first.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as new version
+console.log("Hello World");
+console.log("HI");
+//this was very basic part
+
+/*
+see data types below
+// number=> 2 to power 53
+bigint
+String = ""
+boolean => true/false
+null -> object
+undefined
+symbol
+object
+*/
